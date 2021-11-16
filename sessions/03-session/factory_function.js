f49ec8c95284e3da4b45b92f23777fb5040d6e8a
LuisBalan <jlbalcz@gmail.com>
2021-11-16 13:11:30 -0600

    add examples of factory functions

---
+++ sessions/03-session/factory_function.js
@@ -0,0 +1,38 @@
+function factory(result){
+    console.log('Trabajando...');
+
+    return function(){
+        console.log('resultado', result);
+    }
+}
+
+const r = factory('saludos');
+
+// Una accion que no sabemos cuando terminara exactamente
+function irACasa(callback) {
+    console.log('camino a casa...');
+    console.log('llegue a casa');
+
+    // veamos que es callback
+    console.log(callback);
+    if (callback !== undefined) {
+        callback();
+    } else {
+        console.log('y no hizo nada');
+    }
+}
+
+function avisar() {
+    console.log('ya llegue');
+}
+
+function dormir() {
+    console.log('a mimir');
+}
+
+// Como irian a casa y luego avisarian?
+irACasa(avisar);
+
+// Como dormir y luego ir a casa?
+dormir();
+irACasa();
