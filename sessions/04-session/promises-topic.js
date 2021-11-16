fe4b9debb3e9747f35a6955752802b0f80dadb2e
LuisBalan <jlbalcz@gmail.com>
2021-11-16 12:24:20 -0600

    topic of promises

---
+++ sessions/04-session/promises-topic.js
@@ -0,0 +1,12 @@
+// const promesa = new Promise((resolve, reject) => {
+//     if(success){
+//         resolve(valor)
+//     }else{
+//         reject('error')
+//     }
+// });
+
+// ejemplo Promises
+
+
+
