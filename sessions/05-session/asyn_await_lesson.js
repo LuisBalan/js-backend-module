267055327cd957b5fedd09991dade3fbcb09354e
LuisBalan <jlbalcz@gmail.com>
2021-11-16 23:17:17 -0600

    add asyn and await definitions

---
+++ sessions/05-session/asyn_await_lesson.js
@@ -0,0 +1,7 @@
+async const ejecutaPromesas = () => {
+    const resultado1 = await promesa1();           // tarea larga (asincrono)
+    const resultado2 = 'texto: ' + resultado1;   //tarea corta (sincrono)
+
+    return resultado2;
+
+}
