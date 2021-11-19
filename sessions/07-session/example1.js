240602eef01214c0ab053a52db04995ca1fddfc7
LuisBalan <jlbalcz@gmail.com>
2021-11-19 12:57:32 -0600

    fix http request example

---
+++ sessions/07-session/example1.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const port = 8080
+
+// Creamos un servidor, usando createServer
+const server = http.createServer((request, response) => {
+    const metodo = request.method
+    console.log(metodo)
+    const content = request.content
+    console.log(content)
+
+    const url = request.url;
+    console.log(`URL: ${url}`)
+    
+    // le definimos un todo el header, con writeHead
+    // response.writeHead(200, {})
+    response.statusCode = 202
+    response.setHeader('Content-Type', 'text/json')
+
+    response.write('Hello World!')
+    response.end('{"hola": "a todos"}')
+})
+
+// arrancamos el servidor
+server.listen(port, () => {
+    console.log(`El servidor arranco en el puerto ${port}`)
+})
