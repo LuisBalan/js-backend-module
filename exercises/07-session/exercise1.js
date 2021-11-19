4611a9e314cd0a3c303443d834592ef44016016e
LuisBalan <jlbalcz@gmail.com>
2021-11-19 11:45:10 -0600

    add exercises of 07-session (http requests)

---
+++ exercises/07-session/exercise1.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const port = 8080;
+
+const server = http.createServer((request, response) => {
+    const url = request.url;
+    const metodo = request.method;
+    
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/plain');
+
+    if(url === `/koder` && request.method === 'GET'){
+        response.write('Aquí están todos los koders')
+    }else if(url === '/koder' && request.method === 'POST'){
+        response.write('Aquí puedes crear koders')
+    }else{
+        response.statusCode = 400
+        response.write('Ayuda, no sé qué hacer.')
+    }
+
+    response.end()
+    
+})
+
+server.listen(port, () => {
+    console.log('Server launched at 8080 port.')
+})
