c963e76b73e6d88c53bd5e19e149655207f39065
LuisBalan <jlbalcz@gmail.com>
2021-11-21 09:53:42 -0600

    fix index.js to receive req.body

---
+++ exercises/08-session/02-project-post-plus-filesystem/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+const fs = require('fs/promises');
+const encoding = 'utf-8';
+const bodyParser = require('body-parser');
+// const multer = require('multer');
+// const upload = multer();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/koders', (req, res) => {
+    res.send('Here you can add a new koder');
+    const contenido = req.body;
+    console.log(contenido)
+})
+
+app.listen(port, () => {
+    console.log(`Starting POST endpoint.`)
+})
