667d68056035fb1c234d8a36710596296225eab4
LuisBalan <jlbalcz@gmail.com>
2021-11-21 08:56:36 -0600

    clean script (remove comments)

--- exercises/08-session/01-project/index.js
+++ exercises/08-session/01-project/index.js
@@ -44,30 +44,3 @@ const sendResponse = async (contentToSend) => {
 }
 
 sendResponse('koders.json');
-
-
-// app.get('/koders', (req, res) => {
-//     res.send('Aquí están todos los koders')
-// })
-
-// app.post('/', (req, res) => {
-//     res.send('Aquí puedes crear koders')
-// })
-
-// app.post('/koders', (req, res) => {
-//     res.send('Aquí puedes crear koders')
-// })
-
-// app.put('/koders', (req, res) => {
-//     res.send({response: 'Aquí puedes sustituir un koder'})
-// })
-
-// // funciona igual que el anterior
-// app.put('/koders', (req, res) => {
-//     res.send({response: 'Aquí puedes no sustituir un koder'})
-// })
-
-// //Aquí se inicia su server o app
-// app.listen(port, () => {
-//     console.log(`Example app listening at http://localhost:${port}`)
-// })
