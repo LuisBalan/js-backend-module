81e7a5009192dfee66a622c6c7041f43a031521b
LuisBalan <jlbalcz@gmail.com>
2021-11-26 22:00:46 -0600

    add changes to 13-session-mongoose

--- sessions/13-session-mongoose/index.js
+++ sessions/13-session-mongoose/index.js
@@ -5,12 +5,69 @@ const DB_HOST = 'cluster0.jtdjp.mongodb.net';
 const DB_NAME = 'kodemia';
 const URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
 
+// schema creation
+const koderSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        minlength: 2,
+        maxlength: 100,
+        required: true,
+    }, 
+    lastName:{
+        type: String,
+        minlength: 2,
+        maxlength: 100,
+        required: true,
+    },
+    age: {type: Number, min: 0, max: 120, required: true},
+    gender: {
+        type: String,
+        required: true,
+        maxlength: 1,
+        enum: ['m', 'f', 'x'],
+    }
+});
+
+//Creation of model. Collection name and schema are needed.
+
+const Koder = mongoose.model('koders', koderSchema);
+
 mongoose
     .connect(URL)
-    .then((connection) => {
+    .then(async (connection) => {
         console.log('DB Connected!');
-        console.log(connection);
+        // 
+        
+
+
+
+        // const newKoder = new Koder({
+        //     name: 'Rafa',
+        //     lastName: 'Martinez',
+        //     age: 18,
+        //     gender: 'm'
+        // })
+        // await Koder.create(newKoder)
+
+        await Koder.create({
+            name: 'Marco',
+            lastName: 'Fernandez',
+            age: 18,
+            gender: 'm'
+        })
+
+        await Koder.create({
+            lastName: 'Lopez',
+            age: 18,
+            gender: 'masculino'
+        })
+
+                
+        const koders = await Koder.find({});
+        console.log(koders)
+
     })
     .catch((error) => {
         console.error(error)
     })
+
