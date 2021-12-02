ca5e374ed550d01dd407abbcf3542685b62ef676
LuisBalan <jlbalcz@gmail.com>
2021-12-01 20:24:46 -0600

    add clean architecture session content

---
+++ sessions/16-cleanArch-kodemiaApi/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+// process.env.PORT
+
+const dbConnect = require('./src/lib/db')
+const server = require('./src/lib/server')
+
+dbConnect(process.env)
+    .then(() => {
+        console.log('DB Connected!')
+
+        server.listen(process.env.PORT, () => {
+            console.log('Server is running')
+        })
+    })
+    .catch((error) => {
+        console.error(error)
+    }) 
